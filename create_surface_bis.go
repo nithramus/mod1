48201fc0fd68f0edae5519e3e4821b02ee13bb96
Baptiste ANDRE <bandre@e1r10p18.42.fr>
2018-04-26 17:58:30 +0200

    first push

---
+++ create_surface_bis.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"math"
+	"strconv"
+)
+
+func tri_à_bulle(list_point []point) {
+	var ok int = 1
+	var tmp point
+	for ok == 1 {
+		ok = 0
+		for i := range list_point {
+			print(list_point[i].z, ".z \n")
+			if i != 0 {
+				if list_point[i-1].z < list_point[i].z {
+					tmp = list_point[i-1]
+					list_point[i-1] = list_point[i]
+					list_point[i] = tmp
+					ok = 1
+				}
+			}
+		}
+	}
+}
+
+func string_to_point(list [][]string) []point {
+	var list_point []point
+	for i := range list {
+
+		cor1, _ := strconv.ParseFloat(list[i][1], 64)
+		cor2, _ := strconv.ParseFloat(list[i][2], 64)
+		cor3, _ := strconv.ParseFloat(list[i][3], 64)
+		print(cor1, "\n", cor2, "\n", cor3, "\n")
+		if cor1 == 0 || cor2 == 0 || cor1 > width-1 || cor2 > height-1 {
+			exit("Coordonnée ayant pour valeur 0")
+		}
+		list_point = append(list_point, point{cor1, cor2, cor3})
+	}
+	return list_point
+
+}
+
+func find_dist_most_proche(l_point []point) []int {
+	var proche []int
+	for i := range l_point {
+		yolo := []point{point{0, 0, l_point[i].x}, point{0, 0, l_point[i].y}, point{0, 0, width - l_point[i].x}, point{0, 0, height - l_point[i].y}}
+		tri_à_bulle(yolo)
+		max_dist := yolo[3].z / 2
+		if max_dist*2 > l_point[i].z {
+			max_dist = l_point[i].z
+		}
+		if max_dist < 11 {
+			max_dist = 10
+		}
+		proche = append(proche, int(max_dist))
+	}
+	return proche
+}
+
+func distance(a point, b point) float64 {
+
+	abx := a.x - b.x
+	aby := a.y - b.y
+	return math.Sqrt(abx*abx + aby*aby)
+}
+
+func put_point(pt point, distance_max int, surface *[100][100]float64) {
+	print(distance_max, "\n")
+	var dist float64 = float64(distance_max)
+	startx := int(pt.x - dist + 1)
+	starty := int(pt.y - dist + 1)
+	print(startx)
+	print("\n")
+	print(starty)
+	var t_point point
+	for i := startx; i <= int(pt.x+dist-1); i++ {
+		for j := starty; j <= int(pt.y+dist-1); j++ {
+			t_point = point{float64(i), float64(j), 0.1}
+			distance_actuelle := (float64(distance(pt, t_point)) / (dist))
+			if distance_actuelle <= 1 {
+				surface[i][j] = float64(int(pt.z*(1-(distance_actuelle)*(distance_actuelle)) + distance_actuelle*distance_actuelle*surface[i][j]))
+			}
+
+		}
+	}
+}
+
+func generate_surface(surface *[100][100]float64, l_point []point) {
+	var x int
+	var y int
+	//var z int
+	dist := find_dist_most_proche(l_point)
+	for i := range l_point {
+		x = int(l_point[i].x)
+		y = int(l_point[i].y)
+		//z = int(l_point[i].z)
+		if l_point[i].z >= surface[x][y] {
+			put_point(l_point[i], dist[i], surface)
+		} else {
+			put_point(l_point[i], dist[i], surface)
+		}
+	}
+	// test_suface(surface)
+}
+
+func create_surface_bis(surface *[100][100]float64, test [][]string) {
+	list_point := string_to_point(test)
+	tri_à_bulle(list_point)
+	generate_surface(surface, list_point)
+
+}
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+)
+
+const (
+	height float64 = 100
+	width  float64 = 100
+)
+
+func exit(str string) {
+	fmt.Println(str)
+	os.Exit(0)
+}
+
+func puissance(value int, power int) int {
+	a := 0
+	ret := 1
+
+	for a < power {
+		ret = ret * value
+		a++
+	}
+	return ret
+}
+
+func main() {
+	var surface [100][100]float64
+	var water [100][100]float64
+
+	mode := flag.Int("mode", 0, "Wrong mode value, must be between 1 and 3")
+	name_file := flag.String("file", "", "No file")
+	time := flag.Int("time", 500, "No input time")
+	force := flag.Int("force", 5, "No input force")
+	flag.Parse()
+	file, err := ioutil.ReadFile(*name_file)
+	if *mode == 0 || *name_file == "" {
+		fmt.Println("mod1: usage: ./mod1 -mode [value] -file [file] -time [time] -force [force]")
+		os.Exit(1)
+
+	}
+	if err != nil {
+		print("no such file\n")
+		panic("")
+	}
+	yolo := regexp.MustCompile("^(([[:space:]]*\\(([0-9]+,)([0-9]+,)([0-9]+)\\))+[[:space:]]*)$")
+	if yolo.MatchString(string(file)) == false {
+		fmt.Println("erreur d'input file")
+		os.Exit(1)
+	}
+	re := regexp.MustCompile(" *\\((?:([0-9]+),)(?:([0-9]+),)([0-9]+)\\)")
+	test := re.FindAllStringSubmatch(string(file), -1)
+	create_surface_bis(&surface, test)
+	for x := range water {
+		water[x][0] = 20000
+		water[x][int(width-1)] = 20000
+	}
+	for y := range water[0] {
+		water[0][y] = 20000
+		water[int(height-1)][y] = 20000
+	}
+	if *force < 1 || *force >= 30 {
+		fmt.Println("force must be between 1 and 30")
+	} else if *mode < 1 || *mode > 3 {
+		fmt.Println("Seulement 3 mode, 1, 2 ou 3")
+	} else {
+		fmt.Println("Lancement du programme")
+		draw(&surface, &water, *mode, *time, *force)
+	}
+
+}
